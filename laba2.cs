289b84c017a9a38e22b28a30bb5148f38faa1050
nikitayashny <95637235+nikitayashny@users.noreply.github.com>
2022-10-04 23:48:20 +0300

    add new laba2.cs file with class Date

---
+++ laba2.cs
@@ -0,0 +1,16 @@
+﻿namespace laba2
+{
+    class Date
+    {
+        public int day;
+        public int month;
+        public int year;
+    }
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
