41065ae45282bb442248b5764ac8e9b417c17873
nikitayashny <95637235+nikitayashny@users.noreply.github.com>
2022-10-16 13:04:10 +0300

    split partial class

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    partial class Partial
+    {
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            Random rand = new Random();
+            for (int i = 0; i < name.Length; i++)
+            {
+                hash += name[i];
+            }
+            hash *= rand.Next(1000, 999999);
+            return (int)Math.Abs(hash * name.Length);
+        }
+    }
+}
