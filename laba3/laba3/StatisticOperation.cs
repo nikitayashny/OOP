121c0cdfe90981796b78e98a8061a7fb44639da4
nikitayashny <95637235+nikitayashny@users.noreply.github.com>
2022-10-19 13:09:39 +0300

    add Sum method

---
+++ laba3/laba3/StatisticOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    static class StatisticOperation
+    {
+        public static int Sum(Matrix a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < a.N; j++)
+                {
+                    sum += a[i, j];
+                }
+            }
+            return sum;
+        }
+    }
+}
