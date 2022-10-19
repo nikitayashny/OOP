a258352de239986712062bc070382b8f15fa19a2
nikitayashny <95637235+nikitayashny@users.noreply.github.com>
2022-10-19 13:17:08 +0300

    add Diff method

--- laba3/laba3/StatisticOperation.cs
+++ laba3/laba3/StatisticOperation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +19,22 @@ namespace laba3
                 }
             }
             return sum;
+        }
+        public static int Diff(Matrix a)
+        {
+            int max = a[0, 0];
+            int min = a[0, 0];
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < a.N; j++)
+                {
+                    if (max < a[i, j])
+                        max = a[i, j];
+                    if (min > a[i, j])
+                        min = a[i, j];
+                }
+            }
+            return max - min;
         }
     }
 }
