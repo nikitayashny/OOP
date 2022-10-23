25076fa5b88a60cc5dce8da6ed2d35fdabc9fac6
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-10-23 12:16:29 +0300

    add abstract class, interface, class Caramel

---
+++ laba4/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba4
+{
+    interface IPastry
+    {
+        void Print();
+    }
+    abstract class Candy                                
+    {
+        protected string title;
+        protected int amount;
+        protected int price;
+
+        public Candy(string title, int amount, int price)
+        {
+            this.title = title;
+            this.amount = amount;
+            this.price = price;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        protected int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Информация о товаре:");
+            Console.WriteLine($"Название: {title}");
+            Console.WriteLine($"Количество: {amount}");
+            Console.WriteLine($"Цена: {price}");
+        }
+    }
+    sealed class Caramel : Candy, IPastry
+    {
+        protected string grade;
+        
+        public Caramel(string title, int amount, int price, string grade) : base(title, amount, price)
+        {
+            this.title = title;
+            this.amount = amount;
+            this.price = price;
+            this.grade = grade;
+        }
+
+        public string Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Сорт карамели: {grade}");
+        }
+    }
+    //class CandyBox : Candy
+    //{
+
+    //}
+    //class ChocolateCandy : Candy
+    //{
+
+    //}
+    //class Cookie : Candy
+    //{
+
+    //}
+}
---
+++ laba4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caramel caramel1 = new Caramel("Карамелька", 15, 100, "высший");
+            caramel1.Print();
+            Console.WriteLine();
+        }
+    }
+}
