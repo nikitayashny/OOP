cb664b431d74a4f4da0133cd3b55d622dadbfb1a
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-10-23 13:17:01 +0300

    add is, as, outputs

--- laba4/Program.cs
+++ laba4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +10,34 @@ namespace laba4
     {
         static void Main(string[] args)
         {
-            Caramel caramel1 = new Caramel("Карамелька", 15, 100, "высший");
-            caramel1.Print();
+            Candy Caramel1 = new Caramel("Карамелька", 15, 100, "высший");
+            Caramel caramel1 = Caramel1 as Caramel;
+            if (caramel1 is Caramel)
+            {
+                caramel1.ToString();
+                caramel1.Official();
+            }        
             Console.WriteLine();
+
+            IPastry candybox1 = new CandyBox("Merci", 10, 350, "синий");
+            if (candybox1 is CandyBox)
+            {
+                candybox1.ToString();
+                candybox1.Official();
+            }
+            Console.WriteLine();
+
+            Candy chocolatecandy = new ChocolateCandy("Аришка", 75, 3, 34);
+            if (chocolatecandy is ChocolateCandy chocolatecandy1)
+            {
+                chocolatecandy1.ToString();
+                chocolatecandy1.Official();
+            }
+            Console.WriteLine();
+
+            Cookie cookie1 = new Cookie("Любовь", 40, 85, false);
+            cookie1.ToString();
+            cookie1.Official();
         }
     }
 }
