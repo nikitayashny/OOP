ed5e0ea6a88936029577c24fb686979efc40f491
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-01 14:55:13 +0300

    add enum, struct and partial class

---
+++ laba5/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5
+{
+    partial class Cookie
+    {
+        public override string ToString()
+        {
+            base.ToString();
+            if (gluten == true)
+            {
+                glutenstr = "да";
+            }
+            else
+            {
+                glutenstr = "нет";
+            }
+            Console.WriteLine($"Содержание глютена: {glutenstr}");
+            return "\0";
+        }
+        public override void Official()
+        {
+            Console.WriteLine("<< Очень вкусное печенье >>");
+        }
+    }
+}
---
+++ laba5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            IPastry candybox = new CandyBox("Merci", 10, 350, "синий");
+            if (candybox is CandyBox)
+            {
+                candybox = candybox as CandyBox;
+                candybox.ToString();
+                candybox.GetHashCode();
+                candybox.Official();
+                candybox.Equals(candybox);
+                Console.WriteLine();
+            }
+
+            Candy caramel = new Caramel("Карамелька", 15, 100, "высший");
+            Candy chocolatecandy = new ChocolateCandy("Аришка", 75, 3, 34);
+            Candy cookie = new Cookie("Любовь", 40, 85, false);
+            Cookie? Cookie1 = cookie as Cookie;
+
+            Candy[] candies = { caramel, chocolatecandy, cookie };
+            Console.WriteLine("Вызов метода IAmPrinting"); Console.WriteLine();
+            Printer printer = new Printer();
+            foreach (var item in candies)
+            {
+                printer.IAmPrinting(item);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
