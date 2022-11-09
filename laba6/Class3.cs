01648d08218dad7a427f74d0e56828b83ed554c7
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-10 01:02:08 +0300

    add class exceptions

---
+++ laba6/Class3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace lab04
+{
+    class NullCollectionException : Exception
+    {
+        public NullCollectionException(string message) : base(message) { }
+    }
+    class MaxCollection : OverflowException
+    {
+        public MaxCollection(string message) : base(message) { }
+    }
+    class DeleteNullObject : Exception
+    {
+        public DeleteNullObject(string message) : base(message) { }
+    }
+    class TestNullClass : NullReferenceException
+    {
+        public TestNullClass(string message) : base(message) { }
+    }
+}
