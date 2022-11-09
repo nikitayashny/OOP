027d8bb00da724ce949663af961fac9d6187f8b1
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-10 01:39:48 +0300

    add try catch and exceptions

--- laba6/Program.cs
+++ laba6/Program.cs
@@ -1,22 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace laba5
+namespace laba6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //ChildrensGift childrensgift = new ChildrensGift();
-            //Candy[] candies = { new CandyBox("Merci", 10, 350, 30, "синий"), new Caramel("Карамелька", 15, 100, 90, "высший"),
-            //                    new ChocolateCandy("Аришка", 75, 3, 50, 34), new Cookie("Любовь", 40, 85, 20, false)};
-            //foreach (var i in candies)
-            //{
-            //    childrensgift.addCandy(i);
-            //}
+            ChildrensGift childrensgift = new ChildrensGift();
+            Candy[] candies = { new CandyBox("Merci", 10, 350, 30, "синий"), new Caramel("Карамелька", 15, 100, 90, "высший"),
+                                new ChocolateCandy("Аришка", 75, 3, 50, 34), new Cookie("Любовь", 40, 85, 20, false)};
+            foreach (var i in candies)
+            {
+                childrensgift.addCandy(i);
+            }
             //childrensgift.Show();
             //childrensgift.removeCandy(2);
             //childrensgift.Show();
@@ -31,6 +32,68 @@ namespace laba5
             //ChildrensGift container = new ChildrensGift();
             //Controller.ReadFile(container, @"C:\Users\User\Desktop\ООП\laba5\laba5\ChildrensGift.txt");
             //container.Show();
+
+            try
+            {
+                int test = 2;
+                Logger logger = new Logger();
+                logger.FileLoggerWriteLine("Log-файл успешно создан");
+                try
+                {
+                    ChildrensGift test1 = new ChildrensGift();
+                    
+                    CandyBox candy1 = new CandyBox("Merci", 10, 350, 30, "синий");
+                    for(int i = 0; i < 10; i++)
+                    {
+                        test1.addCandy(candy1);
+                    }
+                    test1.removeCandy(100);
+                    //candies[10].ToString();
+
+                    //int test2 = 0;
+                    //test2 /= 0;
+                    //Debug.Assert(test <= 1 && test >= 0, "Значение testpr может быть только 0 или 1");
+                }
+                catch (DivideByZeroException exception) when (test != 1)
+                {
+                    logger.FileLoggerWriteError("Error NullReferenceException", exception.Message, exception.StackTrace);
+                    logger.ConsoleLoggerError("Error NullReferenceException", exception.Message, exception.StackTrace);
+                }
+                catch (NullCollectionException exception)
+                {
+                    Console.WriteLine($"Произошла ошибка: {exception.Message}");
+                }
+                catch (MaxCollection exception) when (test != 1)
+                {
+                    logger.FileLoggerWriteError("Error Class MaxCollection", exception.Message, exception.StackTrace);
+                    logger.ConsoleLoggerError("Error class MaxCollections", exception.Message, exception.StackTrace);
+                }
+                catch (MaxCollection exception)
+                {
+                    Console.WriteLine($"Произошла ошибка: {exception.Message}");
+                }
+                catch (DeleteNullObject exception)
+                {
+                    Console.WriteLine($"Произошла ошибка: {exception.Message}");
+                }
+                catch
+                {
+                    Console.WriteLine("Поиск возможной причины исключения");
+                    throw;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Попытка обращения к несуществующему элементу");
+            }
+            catch
+            {
+                Console.WriteLine("Исключение необработано");
+            }
+            finally
+            {
+                Console.WriteLine("Тест закончился");
+            }
 
             Console.ReadKey();
         }
