d9076953ea40e417b851f2bad0f6808dd3117b5b
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-23 01:46:11 +0300

    add file Input and Output

--- laba7/Program.cs
+++ laba7/Program.cs
@@ -45,15 +45,47 @@ namespace laba7
                 Caramel caramel3 = new Caramel("Карамель3", 20, 10, "Низший");
 
                 Console.WriteLine(" - - - - - Вывод списка Caramel - - - - - ");
-                List<Caramel> listCheck = new List<Caramel>();
-                listCheck.AddNode(caramel1);
-                listCheck.AddNode(caramel2);
-                listCheck.AddNode(caramel3);
-                listCheck.ShowInfo();
+                List<Caramel> listCaramel = new List<Caramel>();
+
+                listCaramel.AddNode(caramel1);
+                listCaramel.AddNode(caramel2);
+                listCaramel.AddNode(caramel3);
+                listCaramel.ShowInfo();
+
                 Console.WriteLine(" - - - - - Вывод списка без удалённого элемента - - - - - - - ");
-                listCheck.DeleteNode(caramel3);
-                listCheck.ShowInfo();
-                Console.WriteLine("Количество элементов списка Check " + StaticOperations.Count(listCheck));
+                listCaramel.DeleteNode(caramel3);
+                listCaramel.ShowInfo();
+                Console.WriteLine("Количество элементов списка Check " + StaticOperations.Count(listCaramel));
+
+                Console.WriteLine("------------------------------------------");
+                List<Caramel> listCaramel2 = new List<Caramel>();
+
+                //listCaramel2.AddNode(caramel1);
+                //listCaramel2.AddNode(caramel2);
+                //listCaramel2.AddNode(caramel3);
+
+                //Console.WriteLine(listCaramel == listCaramel2);
+
+                List<Caramel> list1 = new List<Caramel>();
+                List<string> list2 = new List<string>();
+                List<int> list3 = new List<int>();
+
+                Streams <Caramel>.ReadFile(ref list1, ref list2, ref list3, @"C:\Users\User\Desktop\ООП\laba7\laba7\out.txt");
+                Console.WriteLine("-------------------------------------------Вывод считанных данных");
+                list1.ShowInfo();
+                list2.ShowInfo();
+                list3.ShowInfo();
+
+
+                List<Caramel> list4 = new List<Caramel>();
+
+                list4.AddNode(new Caramel("Caramel1", 10, 100, "high"));
+                list4.AddNode(new Caramel("Caramel2", 5, 50, "middle"));
+                list4.AddNode(new Caramel("Caramel3", 20, 10, "low"));
+
+                Console.WriteLine("------------------------------------------- Данные которые запишутся в файл");
+                list4.ShowInfo();
+                Streams<Caramel>.InFile(ref list4, @"C:\Users\User\Desktop\ООП\laba7\laba7\in.txt");          
             }
             catch (DeleteNotFounded exception)
             {
