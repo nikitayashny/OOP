0c04f7234251965f91c37148cbfd0ff414438319
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-23 22:35:13 +0300

    made objects and check states

---
+++ laba8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User.UserDelegate subs1;
+            User.UserDelegate subs2;
+            User user = new User();
+
+            int Obj1 = 2004;
+            string Obj2 = "Obj2";
+            char Obj3 = 'a';
+
+            user.Move += obj =>
+            {            
+                Console.WriteLine($"Объект {obj} подписан на Move");
+                Console.WriteLine();
+            };
+
+            user.Squeeze += obj =>
+            {
+                
+                Console.WriteLine($"Объект {obj} подписан на Squeeze");
+                Console.WriteLine();
+            };
+
+            subs1 = user.move;
+            subs1 += user.squeeze;
+            subs1 += user.squeeze;
+
+            subs2 = user.move;
+
+            subs1(Obj1);
+            subs2(Obj2);
+            subs2(Obj3);
+
+            Console.WriteLine("Проверяем состояния объектов:");
+            Console.WriteLine($"{Obj1} {Obj2} {Obj3}");
+        }
+    }
+}
