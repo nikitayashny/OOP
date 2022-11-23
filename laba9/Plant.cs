dc4531a8613d05006079e228542b3e2adc23a2f3
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-24 01:28:11 +0300

    Create class Plant with get set

---
+++ laba9/Plant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba9
+{
+    class Plant
+    {
+        public string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public Plant(string type)
+        {
+            Type = type;
+        }
+        public override string ToString()
+        {
+            return Type;
+        }
+    }
+}
---
+++ laba9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace lab09
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
