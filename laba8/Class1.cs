f43dd1e891285ba54fa5e857f5a12b115f92268a
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-23 22:53:19 +0300

    add removespace and tolower

--- laba8/Class1.cs
+++ laba8/Class1.cs
@@ -1,4 +1,38 @@
-class StringEditor
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace laba8
+{
+    class User
+    {
+        public delegate void UserDelegate(object obj);
+
+        public event UserDelegate Move;
+        public event UserDelegate Squeeze;
+
+        public void move(object obj)
+        {
+            Console.WriteLine("Отработал move");
+            Console.WriteLine("Задайте смещение");
+            int num = int.Parse(Console.ReadLine());
+            obj = num;
+            obj.ToString();
+            Move?.Invoke(obj);
+        }
+        public void squeeze(object obj)
+        {
+            Console.WriteLine("Отработал squeeze");
+            Console.WriteLine("Задайте сжатие");
+            int num = int.Parse(Console.ReadLine());
+            obj.ToString();
+            Squeeze?.Invoke(obj);
+        }
+    }
+    class StringEditor
     {
         public static string RemovePunctuation(string str)
         {
@@ -26,3 +60,4 @@ class StringEditor
             return str.Replace(" ", string.Empty);
         }
     }
+}
