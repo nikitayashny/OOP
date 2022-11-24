d067b299f74aec7ce08d761e3c9ddf0f6c7ef2c6
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-24 16:06:21 +0300

    add universal collections

--- laba9/Program.cs
+++ laba9/Program.cs
@@ -23,6 +23,70 @@ namespace laba9
             plants.ShowCollection();
             Console.WriteLine();
             plants.Search(flower);
+            plants.Add(grass);
+            plants.ShowCollection();
+            Console.WriteLine();
+
+            HashSet<int> evenNumbers = new HashSet<int>();
+            HashSet<int> oddNumbers = new HashSet<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                evenNumbers.Add(i * 2);
+                oddNumbers.Add((i * 2) + 1);
+            }
+
+            Console.Write("evenNumbers содержит {0} элементов: ", evenNumbers.Count);
+            DisplaySet(evenNumbers);
+            Console.Write("oddNumbers содержит {0} элементов: ", oddNumbers.Count);
+            DisplaySet(oddNumbers);
+
+            HashSet<int> allNumbers = new HashSet<int>(evenNumbers);
+
+            Console.WriteLine();
+            Console.WriteLine("Объединение evenNumbers с oddNumers...");
+            allNumbers.UnionWith(oddNumbers);
+
+            Console.Write("allNumbers содержит {0} элементов: ", allNumbers.Count);
+            DisplaySet(allNumbers);
+
+            allNumbers.Remove(5);
+            Console.WriteLine();
+            Console.Write("Коллекция без удалённого элемента:");
+            DisplaySet(allNumbers);
+            Console.WriteLine("allNumbers содержит {0} элементов", allNumbers.Count);
+
+            void DisplaySet(HashSet<int> collection)
+            {
+                Console.Write("{");
+                foreach (int i in collection)
+                {
+                    Console.Write(" {0}", i);
+                }
+                Console.WriteLine(" }");
+            }
+
+            Console.WriteLine();
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (var item in allNumbers)
+            {
+                dict.Add(item, item);
+            }
+            foreach (var pair in dict)
+            {
+                Console.WriteLine($"ключ {pair.Key}, значение {pair.Value}");
+            }
+
+            int pair2;
+
+            if (dict.TryGetValue(6, out pair2))
+            {
+                pair2 = dict[6];
+                Console.WriteLine($"объект :{pair2}- найден");
+            }
+            
         }
     }
 }
