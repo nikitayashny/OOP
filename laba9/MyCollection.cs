241c2b33ec9b00a433f372d6727b15f2011d0947
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-24 13:34:31 +0300

    add interface and methods

--- laba9/MyCollection.cs
+++ laba9/MyCollection.cs
@@ -1,13 +1,50 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace laba9
-{
-    class MyCollection
+{   public interface IList<T>
     {
+        void Add(T item);
+        
+        void Delete(T item);
+        void Search(T item);
+        void ShowCollection();
+    }
 
+    public class MyCollection<T> : IList<T>
+    {
+        private ArrayList items = new ArrayList();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Delete(T item)
+        {
+            items.Remove(item);
+        }
+        public void Search(T item)
+        {
+            if (items.Contains(item))
+            {
+                Console.WriteLine($"{item} найден");
+            }
+            else
+            {
+                Console.WriteLine($"{item} не найден");
+            }
+        }
+        public void ShowCollection()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
