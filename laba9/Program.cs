0df8a9893285bd353865689b7f8abf9657414890
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-11-24 13:58:31 +0300

    add objects Plant and Collection

--- laba9/Program.cs
+++ laba9/Program.cs
@@ -1,16 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
-namespace lab09
+namespace laba9
 {
     class Programm
     {
         static void Main(string[] args)
         {
+            Plant tree = new Plant("Дерево");
+            Plant flower = new Plant("Цветок");
+            Plant grass = new Plant("Трава");
+            Plant bush = new Plant("Куст");
 
+            MyCollection<Plant> plants = new MyCollection<Plant>() { tree, flower, grass, bush};
+
+            plants.ShowCollection();
+            Console.WriteLine();
+            plants.Delete(grass);
+            plants.ShowCollection();
+            Console.WriteLine();
+            plants.Search(flower);
         }
     }
 }
