42a07e0210e302a4fa0d31ec9881d20150e4f0d3
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-12-21 18:33:23 +0300

    Add files via upload

---
+++ laba11/laba11/Candy.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba11
+{
+    interface IPastry
+    {
+        void Print(string message);
+        void ChangePrice(ref int newPrice, out int price);
+    }
+    class Candy : IPastry
+    {
+        const int MAX_ID = 10;
+        static int id;
+        string name;
+        int upc;
+        string producer;
+        int price;
+        int bestBefore;
+        int count;
+        public Candy(string name, int upc, int price, int bestBefore, int count)
+        {
+            if (!string.IsNullOrEmpty(name) && id <= MAX_ID)
+            {
+                this.name = name;
+                this.upc = upc;
+                this.price = price;
+                this.bestBefore = bestBefore;
+                this.count = count;
+                id++;
+            }
+            else
+            {
+                throw new Exception("Cannot create a candy");
+            }
+        }
+        public Candy()
+        {
+            id++;
+        }
+        public Candy(string name, int price, string producer, int upc = 11111, int bestBefore = 11111)
+        {
+            if (!string.IsNullOrEmpty(name) && id <= MAX_ID)
+            {
+                this.name = name;
+                this.producer = producer;
+                this.upc = upc;
+                this.price = price;
+                this.bestBefore = bestBefore;
+                count = 10;
+                id++;
+            }
+            else
+            {
+                throw new Exception("Cannot create a candy");
+            }
+        }
+        public string Producer
+        {
+            get
+            {
+                return producer;
+            }
+            set
+            {
+                producer = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(name))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new Exception("Name is null or empty");
+                }
+            }
+        }
+        public int Upc
+        {
+            get
+            {
+                return upc;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    upc = value;
+                }
+                else
+                {
+                    throw new Exception("Upc is incorrect");
+                }
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new Exception("Price is incorrect");
+                }
+            }
+        }
+        public int BestBefore
+        {
+            get
+            {
+                return bestBefore;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    bestBefore = value;
+                }
+                else
+                {
+                    throw new Exception("BestBefore is incorrect");
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    count = value;
+                }
+                else
+                {
+                    throw new Exception("Count is incorrect");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"name: {Name}, price {Price}, producer {Producer}";
+        }
+        public static void PrintClassInfo()
+        {
+            Console.WriteLine($"Class Info:\nClassName is Candy\nNumber of objects is {id}\nMaximum number is {MAX_ID}");
+        }
+        public string PrintSum()
+        {
+            return $"Whole sum of the candy:\nThe whole sum of {Name} is {Count * Price}\n";
+        }
+        public void ChangePrice(ref int newPrice, out int price)
+        {
+            price = newPrice;
+            Console.WriteLine("New price is " + price);
+        }
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void PrintCandies(List<string> parm)
+        {
+            foreach (var product in parm)
+            {
+                Console.WriteLine(product);
+            }
+
+        }
+    }
+}
---
+++ laba11/laba11/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace laba11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "laba11.Candy";
+            using (StreamWriter sw = new StreamWriter(@"Reflection.txt", false, Encoding.Default))
+            {
+                sw.WriteLine("- - - - - определение имени сборки - - - - -");
+                sw.WriteLine(Reflector.GetAssemblyName());
+                sw.WriteLine("- - - - - публичные конструкторы - - - - -");
+                sw.WriteLine(Reflector.HasPublicConstructors(name));
+                sw.WriteLine("- - - - - все общедоступные публичные методы - - - - -");
+                Reflector.PublicMethods(name, sw);
+                sw.WriteLine("- - - - - информация о полях и свойствах класса - - - - -");
+                Reflector.FieldsAndProperties(name, sw);
+                sw.WriteLine("- - - - - все реализованные классом интерфейсы - - - - -");
+                Reflector.Interfaces(name, sw);
+                sw.WriteLine("- - - - - выводит по имени класса имена методов - - - - -");
+                Reflector.MethodType(name, "String", sw);
+                sw.WriteLine("- - - - - метод Invoke - - - - -");
+                Reflector.Invoke(name, "PrintCandies");
+                sw.WriteLine(Reflector.Create(name).ToString());
+                sw.WriteLine("///////////////////////////////////////////////////////////");
+            }
+
+            name = "laba11.Developer";
+            using (StreamWriter sw = new StreamWriter(@"Reflection.txt", true, Encoding.Default))
+            {
+                sw.WriteLine("- - - - - определение имени сборки - - - - -");
+                sw.WriteLine(Reflector.GetAssemblyName());
+                sw.WriteLine("- - - - - публичные конструкторы - - - - -");
+                sw.WriteLine(Reflector.HasPublicConstructors(name));
+                sw.WriteLine("- - - - - все общедоступные публичные методы - - - - -");
+                Reflector.PublicMethods(name, sw);
+                sw.WriteLine("- - - - - информация о полях и свойствах класса - - - - -");
+                Reflector.FieldsAndProperties(name, sw);
+                sw.WriteLine("- - - - - все реализованные классом интерфейсы - - - - -");
+                Reflector.Interfaces(name, sw);
+                sw.WriteLine("- - - - - выводит по имени класса имена методов - - - - -");
+                Reflector.MethodType(name, "Int32", sw);
+                sw.WriteLine(Reflector.Create(name).ToString());
+                sw.WriteLine("///////////////////////////////////////////////////////////");
+            }
+
+            name = "System.Type";
+            using (StreamWriter sw = new StreamWriter(@"Reflection.txt", true, Encoding.Default))
+            {
+                sw.WriteLine("- - - - - определение имени сборки - - - - -");
+                sw.WriteLine(Reflector.GetAssemblyName());
+                sw.WriteLine("- - - - - публичные конструкторы - - - - -");
+                sw.WriteLine(Reflector.HasPublicConstructors(name));
+                sw.WriteLine("- - - - - все общедоступные публичные методы - - - - -");
+                Reflector.PublicMethods(name, sw);
+                sw.WriteLine("- - - - - информация о полях и свойствах класса - - - - -");
+                Reflector.FieldsAndProperties(name, sw);
+                sw.WriteLine("- - - - - все реализованные классом интерфейсы - - - - -");
+                Reflector.Interfaces(name, sw);
+                sw.WriteLine("- - - - - выводит по имени класса имена методов - - - - -");
+                Reflector.MethodType(name, "value", sw);
+            }
+        }
+    }
+}
