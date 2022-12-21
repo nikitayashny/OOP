cfbdbafac987fc3f2a132a00086643d9cf074ddc
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-12-21 20:41:18 +0300

    add class YNSLog.cs

---
+++ laba12/YNSLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba12
+{
+    public static class YNSLog
+    {
+        private static string _path;
+        public static string Path
+        {
+            set 
+            {
+                _path = value; 
+            }
+            get 
+            {
+                File.AppendAllText(_path, $"{DateTime.Now}\n");
+                return _path;
+            }
+        }
+    }
+}
