d59cbca466260acbf13de2356403563f8eee7423
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2022-12-21 21:16:04 +0300

    create YNSLog.Path, take DiskInfo, take FileInfo

--- laba12/Program.cs
+++ laba12/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace laba12
+namespace laba12
 {
     class Program
     {
@@ -6,7 +6,16 @@
         {
             try
             {
+                YNSLog.Path = "C:\\Users\\User\\Desktop\\ООП\\laba12\\laba12\\ynslogfile.txt";
 
+                YNSDiskInfo.FreeMemory();
+                YNSDiskInfo.WriteFileSystem();
+                YNSDiskInfo.WriteGlobalInf();
+
+                YNSFileInfo.PathToFile = "C:\\Users\\User\\Desktop\\ООП\\задания\\12_Потоки_файловая система.pdf";
+                YNSFileInfo.WriteFullPath(); 
+                YNSFileInfo.WriteFulInf();
+                YNSFileInfo.WritrDopInf();
             }
             catch (Exception e)
             {
