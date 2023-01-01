c10058453b13c498ce61ce65e8db6cf5d693e34b
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-01-01 21:41:00 +0300

    add Task1

--- laba15/Program.cs
+++ laba15/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace laba15
+{
+        private static void Task1()
+        {
+            var task = new Task(() =>
+            {
+                multiplicationMatrix(20);
+            });
+            Console.WriteLine("ID задачи: " + task.Id);
+            Console.WriteLine("Статус задачи до выполнения: " + task.Status);
+            var sw = new Stopwatch();
+            task.Start();
+            sw.Start();
+            Console.WriteLine("Статус задачи во время выполнения: " + task.Status);
+            task.Wait();
+            sw.Stop();
+            Console.WriteLine("Время выполнения: " + sw.Elapsed);//затраченное время
+            sw.Restart();
+            Console.WriteLine("Статус задачи после выполнения: " + task.Status);
+            Console.WriteLine("Время выполнения: " + sw.Elapsed + "\n");//затраченное время
+
+        }
+}
