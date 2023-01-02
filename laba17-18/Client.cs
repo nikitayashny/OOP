680af5ffbdab81f4bb4f2bf07b3de1ff3cb85f49
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-01-02 13:43:35 +0300

    add class Client

---
+++ laba17-18/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17_18
+{
+    internal class Client
+    {
+        public string name;
+
+        public Client(string name)
+        {
+            this.name = name;
+        }
+
+        public void depositMoneyInTheBankAccounts(BankAccount bankAccount, double money)
+        {
+            bankAccount.AddMoney(money);
+        }
+
+        public void makePayment(BankAccount bankAccount, double money)
+        {
+            bankAccount.RemoveMoney(money);
+        }
+        public void BlockedBankAccount(BankAccount bankAccount)
+        {
+            bankAccount.Blocked();
+        }
+    }
+}
