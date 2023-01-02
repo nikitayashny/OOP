60bbeba1f64e28554606fee1b137906a278a8b3e
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-01-02 13:46:34 +0300

    add class AbstractFactory

---
+++ laba17-18/AbstractFactory.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17_18
+{
+    abstract class AbstractFactoryCard
+    {
+        public abstract CurrencyCard CreateGetCurrency();
+        public abstract TypeCard CreateGetType();
+    }
+
+    abstract class CurrencyCard
+    {
+        public abstract void GetCurrency();
+    }
+    abstract class TypeCard
+    {
+        public abstract void GetTypeCard();
+    }
+
+    class Dollar : CurrencyCard
+    {
+        public override void GetCurrency()
+        {
+            Console.WriteLine("Dollar");
+        }
+    }
+
+    class Euro : CurrencyCard
+    {
+        public override void GetCurrency()
+        {
+            Console.WriteLine("Euro");
+        }
+    }
+
+    class Bitcoin : CurrencyCard
+    {
+        public override void GetCurrency()
+        {
+            Console.WriteLine("Bitcoin");
+        }
+    }
+
+    class CreditCard : TypeCard
+    {
+        public override void GetTypeCard()
+        {
+            Console.WriteLine("Credit card");
+        }
+    }
+
+    class DebitCard : TypeCard
+    {
+        public override void GetTypeCard()
+        {
+            Console.WriteLine("Debit Card");
+        }
+    }
+
+    class CreditDollarFactory : AbstractFactoryCard
+    {
+        public override CurrencyCard CreateGetCurrency()
+        {
+            return new Dollar();
+        }
+
+        public override TypeCard CreateGetType()
+        {
+            return new CreditCard();
+        }
+    }
+
+    class CreditBitcoinFactory : AbstractFactoryCard
+    {
+        public override CurrencyCard CreateGetCurrency()
+        {
+            return new Bitcoin();
+        }
+
+        public override TypeCard CreateGetType()
+        {
+            return new CreditCard();
+        }
+    }
+
+    class DebitBitcoinFactory : AbstractFactoryCard
+    {
+        public override CurrencyCard CreateGetCurrency()
+        {
+            return new Bitcoin();
+        }
+
+        public override TypeCard CreateGetType()
+        {
+            return new DebitCard();
+        }
+    }
+
+    class DebitDollarFactory : AbstractFactoryCard
+    {
+        public override CurrencyCard CreateGetCurrency()
+        {
+            return new Dollar();
+        }
+
+        public override TypeCard CreateGetType()
+        {
+            return new DebitCard();
+        }
+    }
+}
