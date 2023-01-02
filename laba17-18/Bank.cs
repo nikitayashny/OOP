07d5ddb9651fa529da0bc1297c33c1b4a7dd42ce
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-01-02 13:45:53 +0300

    add class Bank

---
+++ laba17-18/Bank.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab_17_18
+{
+    internal class Bank
+    {
+        private static int idCount = -1;
+
+        public BankAccount CreateBankAccount(string name)
+        {
+            idCount++;
+            return (new BankAccount(idCount.ToString(), name));
+        }
+    }
+}
