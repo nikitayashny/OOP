6ac3f54ac847ec499bd8ba6a53f510f0a241a4fa
Nikita Yashny <95637235+nikitayashny@users.noreply.github.com>
2023-01-02 13:43:18 +0300

    add class ICard

---
+++ laba17-18/ICard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17_18
+{
+    interface ICard
+    {
+        ICard Clone();
+        void ShowCardInfo();
+    }
+}
